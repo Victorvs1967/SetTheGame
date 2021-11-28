0dd01c08f5da36aa37077d64c20841e96d90d4be
Victor <victorsmirnov@me.com>
2021-11-28 20:25:58 +0200

    Initial Commit

---
+++ SetTheGame/SetTheGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetTheGameApp.swift
+//  SetTheGame
+//
+//  Created by Victor Smirnov on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetTheGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
